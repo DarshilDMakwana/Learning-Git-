f883eb2e146094de716c1660ea29c90053f395d7
Darshil <darshild.1419@gmail.com>
2026-02-13 08:25:02 +0100

    changes in second branch

--- 3.py
+++ 3.py
@@ -3,3 +3,6 @@ print(f"In {ort}. Ich auch")
 
 wann = "Sept 2024"
 print(f"Seit {wann}. Das ist toll.")
+
+was = "Student"
+print(f"Bist du {was}?")
--- 4.py
+++ 4.py
@@ -3,3 +3,6 @@ print(f"{film} ist mein Lieblingsfilm aus allen Filmen.")
 
 essen = "Pizza"
 print(f"{essen} ? Ich esse gerne sie auch")
+
+trink = "Cola"
+print(f"{trink}. Ich mag sie nicht.")
